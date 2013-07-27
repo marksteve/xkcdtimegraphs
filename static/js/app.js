7706d04841fd21d66d5968b6197a993deca665d9
Mark Steve Samson <hello@marksteve.com>
2013-07-27 14:14:42 +0800

    Minor styles and js changes

--- static/js/app.js
+++ static/js/app.js
@@ -2,18 +2,24 @@ $(function() {
 
 $('#gh-repo-events').on('submit', function(e) {
     e.preventDefault();
-    var $form = $(this);
+    var $form = $(this),
+        $graph = $form.next('.graph');
+
     $.ajax({
         url: $form.attr('action'),
         type: 'POST',
         data: $form.serialize()
     }).done(function(response) {
-        $form.next('.graph').empty().append(
-            $('<img>').attr('src', response)
-        );
+        $graph.empty().append($('<img>').attr('src', response));
     }).error(function() {
-        alert('Oops!');
+        $graph.text('Failed to render graph!');
+    }).always(function() {
+        $form.find('input').prop('disabled', false);
     });
+
+    // Loading feedback
+    $graph.html('Loading&hellip;');
+    $form.find('input').prop('disabled', true);
 });
 
 });
